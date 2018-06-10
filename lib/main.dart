5c50dcd6d71bd021c030fbbde5b826b21fb77643
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2018-06-10 13:32:47 -0400

    Added Google Authentication and HomeView

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'views/about.dart';
 import 'views/goals.dart';
 import 'views/home.dart';
 import 'views/log.dart';
 import 'views/supervisors.dart';
+
+/// Enum representing the different sign-in options.
+enum _SignInOptions {
+  google,
+  email,
+  facebook
+}
 
 /// Main function, creates a new [MaterialApp] whose home widget is [PermitLog].
 void main() {
@@ -25,11 +36,20 @@ class PermitLog extends StatefulWidget {
 
 /// [State] for the [PermitLog] widget.
 class _PermitLogState extends State<PermitLog> {
+  /// Firebase Auth API Interface
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  /// Firebase User Object
+  FirebaseUser _curUser;
+  /// Create GoogleSignIn instance that allows us to access their e-mail and OpenID.
+  final GoogleSignIn _googleSignIn = new GoogleSignIn(
+    scopes: ['openid', 'email']
+  );
+
   /// Style for [Drawer] menu items.
   final TextStyle menuText = new TextStyle(color: Colors.white);
 
   /// Content for the current state.
-  var title = 'Home';
+  String title = 'Home';
   Widget content = new HomeView();
 
   /// Sets the [AppBar]'s title and navigates to the view indicated.
@@ -59,6 +79,93 @@ class _PermitLogState extends State<PermitLog> {
     Navigator.pop(context);
   }
 
+  /// Tells the user that authentication failed and they need to try again.
+  void _tryAuthenticationAgain(BuildContext context) {
+    Scaffold.of(context).showSnackBar(new SnackBar(
+        content: new Text("Authentication failed. Please try again")
+    ));
+  }
+
+  /// Authenticates the user using Google sign-in.
+  Future<Null> _authenticateUserGoogle(BuildContext context) async {
+    /// This object represents the user in the Google API.
+    GoogleSignInAccount googleUser = _googleSignIn.currentUser;
+
+    /// If the user is not signed in, try to re-authenticate the user:
+    if (googleUser == null) googleUser = await _googleSignIn.signInSilently();
+    /// If this fails, sign the user in using an interactive dialog:
+    if (googleUser == null) {
+      /// Keep trying to get the user's authentication until it works.
+      while (googleUser == null) {
+        /// Start the interactive sign-in process.
+        googleUser = await _googleSignIn.signIn();
+
+        /// If authentication failed, tell the user to try again.
+        if (googleUser == null) _tryAuthenticationAgain(context);
+      }
+    }
+
+    /// Get the object with the user's data.
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    /// Get the FirebaseUser from the googleAuth object.
+    _auth.signInWithGoogle(
+        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken
+    ).then(
+      /// Set _curUser inside setState to update the widget:
+      (FirebaseUser user) =>
+      setState(() {
+        _curUser = user;
+        print("signed in " + _curUser?.uid.toString());
+      })
+    );
+  }
+
+  /// Authenticates the user using one of the sign-in options.
+  Future<Null> _authenticateUser(BuildContext context) async {
+    /// First, try to get the user from FirebaseAuth:
+    FirebaseUser user = await _auth.currentUser();
+    /// If user is non-null, update _curUser and return:
+    if (user != null) {
+      setState(() { _curUser = user; });
+      return;
+    }
+    /// Otherwise, we need to sign the user in.
+
+    /// Ask the user to select a sign-in option.
+    _SignInOptions option = await showDialog<_SignInOptions>(
+      context: context,
+      builder: (BuildContext context) => new AlertDialog(
+        content: new Text("Select a sign-in option."),
+        actions: <Widget>[
+          new FlatButton(
+              child: new Text("Google"),
+              onPressed: () => Navigator.pop(context, _SignInOptions.google)
+          ),
+          new FlatButton(
+              child: new Text("E-mail"),
+              onPressed: () => Navigator.pop(context, _SignInOptions.email)
+          ),
+          new FlatButton(
+              child: new Text("Facebook"),
+              onPressed: () => Navigator.pop(context, _SignInOptions.facebook)
+          ),
+        ]
+      )
+    );
+    /// Call different authentication methods depending on what is chosen:
+    switch (option) {
+      case _SignInOptions.google:
+        _authenticateUserGoogle(context);
+        break;
+      case _SignInOptions.email:
+        /// Coming soon!
+        break;
+      case _SignInOptions.facebook:
+        /// Coming soon!
+        break;
+    }
+  }
+
   /// Builds the current state.
   @override
   Widget build(BuildContext context) {
@@ -67,7 +174,16 @@ class _PermitLogState extends State<PermitLog> {
         title: new Text(this.title),
         backgroundColor: Colors.blueAccent,
       ),
-      body: this.content,
+      /// Authenticate the user inside a Builder
+      /// so that the context has access to the Scaffold.
+      body: new Builder(
+        builder: (BuildContext context) {
+          /// Try to authenticate the user as soon as possible:
+          if (_curUser == null) _authenticateUser(context);
+
+          return this.content;
+        }
+      ),
       drawer: new Drawer(
         child: new Container(
           color: Colors.blueAccent,
@@ -102,6 +218,8 @@ class _PermitLogState extends State<PermitLog> {
                 leading: new Icon(Icons.exit_to_app, color: Colors.white,),
                 title: new Text("Sign Out", style: this.menuText,),
                 onTap: () {
+                  /// Sign the user out, reset _curUser, and call setState:
+                  _auth.signOut().then((e) => setState(() { _curUser = null; }));
                   Navigator.pop(context);
                 },
               ),
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 /// View that serves as the home screen for the PermitLog app. Displays
 /// the current drive timer and totals for the user's goals.
@@ -9,10 +12,83 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  /// Firebase Auth API Interface
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  /// Subscription that listens for changes to authentication:
+  StreamSubscription<FirebaseUser> _authSubscription;
+  /// Firebase User Object
+  FirebaseUser _curUser;
+
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("changeme"),
-    );
+    /// Get the TextTheme so we can style the texts:
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    /// Only enable the start button if we know who the user is:
+    VoidCallback startCallback;
+    if (_curUser != null) {
+      startCallback = () {};
+    }
+    /// Subscribe to changes to authentication if we haven't already:
+    if (_authSubscription == null) {
+      _authSubscription = _auth.onAuthStateChanged.listen((FirebaseUser user) =>
+          /// Update _curUser according to the subscription:
+          setState(() {
+            _curUser = user;
+          }));
+    }
+
+    return new SingleChildScrollView(
+        child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              new Card(
+                  child: new Column(
+                      children: <Widget>[
+                        new Text("00:00:00", textAlign: TextAlign.center, style: textTheme.headline),
+                        new Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new RaisedButton(
+                                  onPressed: startCallback,
+                                  child: new Text("Start Drive"),
+                                  color: Theme.of(context).buttonColor
+                              ),
+                              new RaisedButton(
+                                  onPressed: null,
+                                  child: new Text("Stop Drive"),
+                                  color: Theme.of(context).buttonColor
+                              )
+                            ]
+                        ),
+                        new DropdownButton<num>(
+                            value: -1,
+                            items: [new DropdownMenuItem<num>(value: -1, child: new Text("Select a Driver"))],
+                            onChanged: (num index) {}
+                        )
+                      ]
+                  )
+              ),
+              new Card(
+                  child: new Column(
+                    children: <Widget>[
+                      new Text("Time Completed", style: textTheme.headline),
+                      new Text("Total: 00:00/00:00", style: textTheme.headline),
+                      new Text("Day: 00:00/00:00", style: textTheme.headline),
+                      new Text("Night: 00:00/00:00", style: textTheme.headline),
+                      new Text("Weather: 00:00/00:00", style: textTheme.headline),
+                      new Text("Adverse: 00:00/00:00", style: textTheme.headline)
+                    ],
+                  )
+              )
+            ]
+        )
+      );
+  }
+
+  /// When we are done with this widget, cancel the auth subscription.
+  @override
+  void dispose() {
+    _authSubscription.cancel();
+    super.dispose();
   }
 }
