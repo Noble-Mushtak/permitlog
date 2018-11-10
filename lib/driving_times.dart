cc21d4b027d39a96c3e08463c4add8103cc690d0
brianjaustin <brianjaustin@gmail.com>
2018-11-10 16:08:52 -0500

    unittests for DrivingTimes

--- lib/driving_times.dart
+++ lib/driving_times.dart
@@ -1,53 +1,66 @@
 import 'package:firebase_database/firebase_database.dart';
 
-/// Class that holds different amounts of driving times for different categories.
+/// Represents different amounts of driving times for different categories.
+/// Valid categories are given by the list of [types].
 class DrivingTimes {
   /// List holding categories of driving times.
-  static final List<String> TIME_TYPES = <String>["total", "day", "night", "weather", "adverse"];
+  static final List<String> types = <String>[
+    "total",
+    "day",
+    "night",
+    "weather",
+    "adverse"
+  ];
+
   /// Corresponding List holding the times for each category.
   List<int> _drivingTimes = [0, 0, 0, 0, 0];
 
-  /// Constructor
-  DrivingTimes({int total = 0, int day = 0, int night = 0, int weather = 0, int adverse = 0}) {
-    /// Create a corresponding list of the driving times.
+  /// Constructs a new [DrivingTimes] object. Users can optionally specify
+  /// times for the categories listed in [types].
+  DrivingTimes({total = 0, day = 0, night = 0, weather = 0, adverse = 0}) {
     _drivingTimes = [total, day, night, weather, adverse];
   }
 
-  /// Gets the time corresponding to a type.
+  /// Gets the time corresponding to the given type. Throws an [ArgumentError]
+  /// if the given type is not one of the valid [types].
   int getTime(String type) {
-    /// Find the type in TIME_TYPES.
-    int typeIndex = TIME_TYPES.indexOf(type);
-    /// Throw an error if the type is not found.
-    if (typeIndex == -1) throw("Invalid driving time type: "+type);
-    /// Return the corresponding time.
+    int typeIndex = types.indexOf(type);
+    if (typeIndex == -1) {
+      throw new ArgumentError("Invalid driving time type: " + type);
+    }
     return _drivingTimes[typeIndex];
   }
 
-  /// Sets the time corresponding to a type.
+  /// Sets the time corresponding to the given type. Throws an [ArgumentError]
+  /// if the given type is not one of the valid [types].
   void setTime(String type, int time) {
-    /// Find the type in TIME_TYPES.
-    int typeIndex = TIME_TYPES.indexOf(type);
-    /// Throw an error if the type is not found.
-    if (typeIndex == -1) throw("Invalid driving time type: "+type);
-    /// Set the corresponding time.
+    int typeIndex = types.indexOf(type);
+    if (typeIndex == -1) {
+      throw new ArgumentError("Invalid driving time type: " + type);
+    }
+    if (time < 0) {
+      throw new ArgumentError("Driving time must be non-negative.");
+    }
+
     _drivingTimes[typeIndex] = time;
   }
 
-  /// Adds a certain time to the total for a type.
+  /// Adds the time to the total for the given type.
   void addTime(String type, int time) {
-    setTime(type, getTime(type)+time);
+    setTime(type, getTime(type) + time);
   }
 
-  /// Updates the DrivingTimes object based off a Firebase event from the user's goals.
+  /// Updates the DrivingTimes object based off a Firebase event from
+  /// the user's goals.
   void updateWithEvent(Event event) {
-    /// Get the data and go through every type of goal.
-    /// (If the user has not set goals yet, use an empty Map.)
+    // Get the data and go through every type of goal.
     Map data = event.snapshot.value ?? new Map();
-    for (String type in DrivingTimes.TIME_TYPES) {
-      /// If the user has this goal, update that goal type.
-      if (data.containsKey(type)) setTime(type, data[type]);
-      /// Otherwise, set the goal to 0.
-      else setTime(type, 0);
+
+    for (String type in DrivingTimes.types) {
+      if (data.containsKey(type))
+        setTime(type, data[type]);
+      else
+        setTime(type, 0);
     }
   }
 }
---
+++ test/driving_times_test.dart
@@ -0,0 +1,80 @@
+import 'package:test/test.dart';
+
+import 'package:permitlog/driving_times.dart';
+
+void main() {
+  DrivingTimes dt1, dt2;
+
+  setUp(() {
+    dt1 = new DrivingTimes();
+    dt2 = new DrivingTimes(total: 1, day: 7, night: 9, weather: 2, adverse: 3);
+  });
+
+  test("correct types", () {
+    expect(DrivingTimes.types, ["total", "day", "night", "weather", "adverse"]);
+  });
+
+  test("get invalid time type", () {
+    expect(() => dt1.getTime("foo"), throwsArgumentError);
+  });
+
+  test("set invalid time type", () {
+    expect(() => dt1.setTime("bar", 5), throwsArgumentError);
+  });
+
+  test("set invalid time", () {
+    expect(() => dt1.setTime("total", -1), throwsArgumentError);
+    expect(dt1.getTime("total"), 0);
+  });
+
+  test("get/set valid time types", () {
+    // Initial expectations
+    expect(dt1.getTime("total"), 0);
+    expect(dt1.getTime("day"), 0);
+    expect(dt1.getTime("night"), 0);
+    expect(dt1.getTime("weather"), 0);
+    expect(dt1.getTime("adverse"), 0);
+    expect(dt2.getTime("total"), 1);
+    expect(dt2.getTime("day"), 7);
+    expect(dt2.getTime("night"), 9);
+    expect(dt2.getTime("weather"), 2);
+    expect(dt2.getTime("adverse"), 3);
+    // Set some times
+    dt1.setTime("total", 55);
+    dt1.setTime("day", 108);
+    dt1.setTime("night", 2 * 10 ^ 5);
+    dt1.setTime("weather", 1);
+    dt1.setTime("adverse", 42);
+    dt2.setTime("total", 55);
+    dt2.setTime("day", 108);
+    dt2.setTime("weather", 1);
+    dt2.setTime("adverse", 42);
+    // Final expectations
+    expect(dt1.getTime("total"), 55);
+    expect(dt1.getTime("day"), 108);
+    expect(dt1.getTime("night"), 2 * 10 ^ 5);
+    expect(dt1.getTime("weather"), 1);
+    expect(dt1.getTime("adverse"), 42);
+    expect(dt2.getTime("total"), 55);
+    expect(dt2.getTime("day"), 108);
+    expect(dt2.getTime("night"), 9);
+    expect(dt2.getTime("weather"), 1);
+    expect(dt2.getTime("adverse"), 42);
+  });
+
+  test("add invalid time type", () {
+    expect(() => dt2.addTime("baz", 18), throwsArgumentError);
+  });
+
+  test("add invalid time", () {
+    expect(() => dt2.addTime("schlecht", -1), throwsArgumentError);
+  });
+
+  test("add valid time", () {
+    expect(dt2.getTime("total"), 1);
+    expect(dt2.getTime("day"), 7);
+    dt2.addTime("total", 12);
+    expect(dt2.getTime("total"), 13);
+    expect(dt2.getTime("day"), 7);
+  });
+}
