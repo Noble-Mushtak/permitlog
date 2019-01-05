316bc3b494edeceaba378998c8708c74a2d9a7ce
Brian Austin <brianjaustin@gmail.com>
2019-01-05 18:14:18 -0500

    Refactor for new Dart/Flutter version

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,71 +1,77 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-/// Returns a String in H:MM format based off milliseconds.
-String formatMilliseconds(int mseconds) {
+/// Returns a String in H:MM format based on the given number of milliseconds.
+/// Throws an [ArgumentError] if the number of milliseconds is negative.
+String formatMilliseconds(int milliseconds) {
+  if (milliseconds < 0) {
+    throw new ArgumentError("Number of milliseconds must be positive.");
+  }
   NumberFormat twoDigitFormat = new NumberFormat("00");
-  Duration duration = new Duration(milliseconds: mseconds);
+  Duration duration = new Duration(milliseconds: milliseconds);
   int minutes = duration.inMinutes % Duration.minutesPerHour;
   return "${duration.inHours}:${twoDigitFormat.format(minutes)}";
 }
 
 /// Gets reference to data for current learner.
-DatabaseReference getCurrentLearnerRef(DatabaseReference userRef, String learnerKey) {
-  /// If this is the default learner, just return userRef.
+DatabaseReference getCurrentLearnerRef(
+    DatabaseReference userRef, String learnerKey) {
+  // If this is the default learner, just return userRef.
   if (learnerKey.isEmpty) return userRef;
-  /// Otherwise, return the appropriate subchild of learners.
+  // Otherwise, return the appropriate subchild of learners.
   return userRef.child("learners").child(learnerKey);
 }
 
-/// Checks if log has all necessary data.
+/// Checks if the give log is valid. A valid log consists of a [Map] containing
+/// the keys `start`, `end`, `night`, and `driver_id`. Each represents
+/// an attributed of a logged drive.
 bool logIsValid(dynamic logData) {
   if (logData is Map) {
-    /// Make sure start, end, night, driver_id keys are present.
-    return logData.containsKey("start") && logData.containsKey("end")
-        && logData.containsKey("night") && logData.containsKey("driver_id");
+    return logData.containsKey("start") &&
+        logData.containsKey("end") &&
+        logData.containsKey("night") &&
+        logData.containsKey("driver_id");
   }
-  /// If it's not a Map, it's not valid.
   return false;
 }
 
-/// Checks if some data about a person has a complete name.
+/// Checks if the given data about a person has a complete name.
+/// A complete name occurs when the given data is a [Map] with key `name`,
+/// containing another [Map] with the keys `first` and `last`.
 bool hasCompleteName(dynamic personData) {
-  /// If this is a Map and personData["name"] is a Map,
-  /// then check if the "first" and "last" keys are present.
   if (personData is Map && (personData["name"] is Map)) {
-    return personData["name"].containsKey("first")
-        && personData["name"].containsKey("last");
+    return personData["name"].containsKey("first") &&
+        personData["name"].containsKey("last");
   }
-  /// Otherwise, just return false.
-  else return false;
+  return false;
 }
 
-/// Creates name based off data about person.
+/// Returns the full name from the given person data. If the data does not have
+/// a complete name, see [hasCompleteName], throws an [ArgumentError].
 String createName(Map personData) {
-  return personData["name"]["first"]+" "+personData["name"]["last"];
+  if (!hasCompleteName(personData)) {
+    throw new ArgumentError("Given person data does not contain a full name.");
+  }
+  return "${personData["name"]["first"]} ${personData["name"]["last"]}";
 }
 
-/// Helper method that shows non-null messages to user on SnackBar
+/// Helper method that shows the given message to user on a [SnackBar] if
+/// the message is not null.
 void showNonEmptyMessage(BuildContext context, String message) {
-  /// Remove current message
+  // Remove current message
   Scaffold.of(context).removeCurrentSnackBar();
-  /// Don't show anything if message is null.
+  // Display the new message as a SnackBar if not null
   if (message == null) return;
-  /// Show message
   Scaffold.of(context).showSnackBar(new SnackBar(content: new Text(message)));
 }
 
-/// Returns a list of menu items that show each String in data using child,
-/// but have the value of the index of that String in data
+/// Converts the given List of Strings into a List of [DropdownMenuItem].
+/// The value of each item is its index in the original list.
 List<DropdownMenuItem<num>> createDropdownItems(List<String> data) {
   List<DropdownMenuItem<num>> items = [];
-  /// Add an item for each String in data.
   for (int i = 0; i < data.length; i++) {
-    items.add(
-      new DropdownMenuItem<num>(value: i, child: new Text(data[i]))
-    );
+    items.add(DropdownMenuItem<num>(value: i, child: Text(data[i])));
   }
   return items;
 }
